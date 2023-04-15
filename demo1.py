e145bc2b59f34c970ac6c33d36dcb1fc80e00845
khalil <khalillakhdharatc@gmail.com>
2023-04-15 11:17:11 +0100

    Initial commit

---
+++ demo1.py
@@ -0,0 +1,9 @@
+# importer cv2 python 
+import cv2
+# lecture d'image cv2 python
+img=cv2.imread("images.jpeg")
+print(img)
+cv2.imshow("image",img)
+cv2.waitKey(5)
+# destruction de la fenetre cv2 python
+#cv2.destroyAllWindows()
